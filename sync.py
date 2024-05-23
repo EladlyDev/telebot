2f9ebc749ed17c3cf01dd0b303953e0e4236aa94
Mohamed Eladly <msrtv365@yahoo.com>
2024-05-23 13:35:03 +0300

    Fixed the message doesn't sync sometimes and the messages gets repeated sometimes

--- sync.py
+++ sync.py
@@ -1,33 +1,14 @@
-#!/usr/bin/python3
 import asyncio
 import logging
 from telethon import TelegramClient, events
 from telethon.tl.types import MessageMediaPhoto, MessageMediaDocument
+from telethon.errors import ChatAdminRequiredError, ChannelPrivateError, ChatWriteForbiddenError
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class SyncBot:
-    """
-    A Telegram bot that synchronizes messages between different channels.
-
-    Args:
-        api_id (int): The API ID of the Telegram application.
-        api_hash (str): The API hash of the Telegram application.
-        phone_number (str): The phone number associated with the Telegram account.
-        session_path (str): The path to store the session file.
-
-    Attributes:
-        api_id (int): The API ID of the Telegram application.
-        api_hash (str): The API hash of the Telegram application.
-        phone_number (str): The phone number associated with the Telegram account.
-        session_path (str): The path to store the session file.
-        client (TelegramClient): The Telegram client instance.
-        links (dict): A dictionary mapping source usernames or IDs to target usernames or IDs.
-        username_to_id (dict): A dictionary mapping usernames to their corresponding IDs.
-    """
-
     def __init__(self, api_id, api_hash, phone_number, session_path):
         self.api_id = api_id
         self.api_hash = api_hash
@@ -35,95 +16,99 @@ class SyncBot:
         self.session_path = session_path
         self.client = TelegramClient(session_path, api_id, api_hash)
         self.links = {}
-        self.username_to_id = {}
+        self.entity_map = {}
+        self.handler_added = False
 
     async def start_client(self):
-        """
-        Start the Telegram client.
-        """
         await self.client.start(self.phone_number)
 
     def set_links(self, links):
-        """
-        Set the links between source and target usernames or IDs.
-
-        Args:
-            links (dict): A dictionary mapping source usernames or IDs to target usernames or IDs.
-        """
         self.links = links
         logger.info(f"Links set: {self.links}")
 
     async def start_bot(self):
-        """
-        Start the bot and add the event handler for new messages.
-        """
         await self.start_client()
-        await self.map_usernames_to_ids()
-        self.client.add_event_handler(self.handle_new_message, events.NewMessage(chats=list(self.username_to_id.values())))
-        logger.info("Bot started and event handler added.")
+        await self.map_usernames_to_entities()
+        if not self.handler_added:
+            self.client.add_event_handler(self.handle_new_message, events.NewMessage(chats=[entity.id for entity in self.entity_map.values()]))
+            self.handler_added = True
+            logger.info("Event handler added.")
+        logger.info("Bot started.")
         await self.client.run_until_disconnected()
 
     async def stop_bot(self):
-        """
-        Stop the bot and disconnect the client.
-        """
         await self.client.disconnect()
         logger.info("Bot stopped.")
 
-    async def map_usernames_to_ids(self):
-        """
-        Map source usernames or IDs to their corresponding IDs.
-        """
+    async def map_usernames_to_entities(self):
+        dialogs = await self.client.get_dialogs()
+        for dialog in dialogs:
+            if dialog.is_channel or dialog.is_group:
+                self.entity_map['-100' + str(dialog.entity.id)] = dialog.entity
+                if dialog.entity.username:
+                    self.entity_map[f"@{dialog.entity.username}"] = dialog.entity
+
         for identifier in self.links.keys():
             if identifier.startswith('@'):
                 entity = await self.client.get_entity(identifier)
-                self.username_to_id[identifier] = entity.id
+                self.entity_map[identifier] = entity
             else:
-                self.username_to_id[identifier] = int(identifier.replace('-', '')[2:])
-        logger.info(f"Username/ID to ID mapping: {self.username_to_id}")
+                entity_id = int(identifier.replace('-', '')[2:])
+                if str(entity_id) not in self.entity_map:
+                    entity = await self.client.get_entity(entity_id)
+                    self.entity_map[str(entity_id)] = entity
+
+        for targets in self.links.values():
+            for t_identifier in targets:
+                if t_identifier.startswith('@'):
+                    entity = await self.client.get_entity(t_identifier)
+                    self.entity_map[t_identifier] = entity
+                else:
+                    entity_id = int(t_identifier.replace('-', '')[2:])
+                    if str(entity_id) not in self.entity_map:
+                        entity = await self.client.get_entity(entity_id)
+                        self.entity_map[str(entity_id)] = entity
+
+        logger.info(f"Username/ID to entity mapping: {self.entity_map}")
 
     async def handle_new_message(self, event):
-        """
-        Handle new incoming messages and forward them to the target usernames or IDs.
-
-        Args:
-            event (telethon.events.NewMessage.Event): The new message event.
-        """
         message = event.message
         source_id = message.peer_id.channel_id
 
         for identifier, target_identifiers in self.links.items():
-            if self.username_to_id[identifier] == source_id:
+            if str(self.entity_map[identifier].id) == str(source_id):
                 for target_identifier in target_identifiers:
+                    target_entity = self.entity_map[target_identifier]
                     try:
                         logger.info(f"Forwarding message from {identifier} to {target_identifier}")
                         if message.is_reply:
                             reply_message = await message.get_reply_message()
-
                             if reply_message.media:
                                 if isinstance(reply_message.media, MessageMediaPhoto):
-                                    reply_sent_message = await self.client.send_file(target_identifier, reply_message.media, caption=reply_message.message)
+                                    reply_sent_message = await self.client.send_file(target_entity, reply_message.media, caption=reply_message.message)
                                 elif isinstance(reply_message.media, MessageMediaDocument):
-                                    reply_sent_message = await self.client.send_file(target_identifier, reply_message.media, caption=reply_message.message)
+                                    reply_sent_message = await self.client.send_file(target_entity, reply_message.media, caption=reply_message.message)
                                 else:
-                                    reply_sent_message = await self.client.send_message(target_identifier, reply_message.message)
+                                    reply_sent_message = await self.client.send_message(target_entity, reply_message.message)
                             else:
-                                reply_sent_message = await self.client.send_message(target_identifier, reply_message.message)
+                                reply_sent_message = await self.client.send_message(target_entity, reply_message.message)
 
                             if message.media:
                                 if isinstance(message.media, MessageMediaPhoto):
-                                    await self.client.send_file(target_identifier, message.media, caption=message.message, reply_to=reply_sent_message.id)
+                                    await self.client.send_file(target_entity, message.media, caption=message.message, reply_to=reply_sent_message.id)
                                 elif isinstance(message.media, MessageMediaDocument):
-                                    await self.client.send_file(target_identifier, message.media, caption=message.message, reply_to=reply_sent_message.id)
+                                    await self.client.send_file(target_entity, message.media, caption=message.message, reply_to=reply_sent_message.id)
                             else:
-                                await self.client.send_message(target_identifier, message.message, reply_to=reply_sent_message.id)
+                                await self.client.send_message(target_entity, message.message, reply_to=reply_sent_message.id)
                         else:
                             if message.media:
                                 if isinstance(message.media, MessageMediaPhoto):
-                                    await self.client.send_file(target_identifier, message.media, caption=message.message)
+                                    await self.client.send_file(target_entity, message.media, caption=message.message)
                                 elif isinstance(message.media, MessageMediaDocument):
-                                    await self.client.send_file(target_identifier, message.media, caption=message.message)
+                                    await self.client.send_file(target_entity, message.media, caption=message.message)
                             else:
-                                await self.client.send_message(target_identifier, message.message)
+                                await self.client.send_message(target_entity, message.message)
+                    except (ChatAdminRequiredError, ChannelPrivateError, ChatWriteForbiddenError) as e:
+                        logger.error(f"Permission issue: Cannot forward message from {identifier} to {target_identifier}: {e}")
                     except Exception as e:
                         logger.error(f"Failed to forward message from {identifier} to {target_identifier}: {e}")
